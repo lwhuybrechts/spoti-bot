36c30a9367ded1366d965cf725a5bbde59e3caa0
Henk <lwhuybrechts@gmail.com>
2020-12-21 19:28:03 +0100

    ignore chatmember dubplicate properties

--- Spoti-bot/Bot/Chats/ChatMember.cs
+++ Spoti-bot/Bot/Chats/ChatMember.cs
@@ -4,12 +4,14 @@ namespace Spoti_bot.Bot.Chats
 {
     public class ChatMember : TableEntity
     {
+        [IgnoreProperty]
         public long ChatId
         {
             get { return long.Parse(PartitionKey); }
             set { PartitionKey = value.ToString(); }
         }
 
+        [IgnoreProperty]
         public long UserId
         {
             get { return long.Parse(RowKey); }
